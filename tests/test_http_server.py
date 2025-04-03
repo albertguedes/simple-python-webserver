f30d29ace6eafae61862731b2c71913d6b2e6a48
albert r. carnier guedes <albert@teko.net.br>
2025-04-03 10:50:48 -0300

    change: include doc on test.

--- tests/test_http_server.py
+++ tests/test_http_server.py
@@ -6,12 +6,27 @@ from http.server import HTTPServer, BaseHTTPRequestHandler
 
 class TestHTTPServer(unittest.TestCase):
     def test_server_start(self):
-        server = HTTPServer(('localhost', 8000), BaseHTTPRequestHandler)
+        """
+        Test if the server starts correctly on the specified port.
+
+        This test initializes an HTTPServer instance on localhost at port 9000 and
+        handles a single request. It asserts that the server's port is correctly set 
+        to 9000, indicating that the server has started on the desired port.
+        """
+        server = HTTPServer(('localhost', 9000), BaseHTTPRequestHandler)
         server.handle_request()
-        self.assertTrue(server.server_port == 8000)
+        self.assertTrue(server.server_port == 9000)
 
     def test_server_stop(self):
-        server = HTTPServer(('localhost', 8000), BaseHTTPRequestHandler)
+        """
+        Test if the server stops correctly on the specified port.
+
+        This test initializes an HTTPServer instance on localhost at port 9000,
+        handles a single request, and then stops the server with
+        server.server_close(). It asserts that the server's port is correctly set
+        to 0, indicating that the server has stopped on the desired port.
+        """
+        server = HTTPServer(('localhost', 9000), BaseHTTPRequestHandler)
         server.handle_request()
         server.server_close()
         self.assertTrue(server.server_port == 0)
